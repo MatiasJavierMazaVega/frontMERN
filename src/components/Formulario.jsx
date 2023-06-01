0d002356ef8a68151f2e8909ce10488c405ddc35
MatiasMV <mjmazavega@gmail.com>
2023-06-01 17:46:47 -0300

    deploy front

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,122 @@
+import 'semantic-ui-css/semantic.min.css';
+import { Form, Button } from "semantic-ui-react";
+import { useForm } from "react-hook-form";
+import  axios  from "axios";
+
+
+
+const Formulario = () =>{
+
+    const URL = process.env.REACT_APP_URL;
+
+    const {register, handleSubmit, reset, formState: {errors} } = useForm({
+        defaultValues: {
+            nombre:'',
+            apellido:'',
+            email:'',
+            password:''
+            
+        }
+    });
+
+    const onSubmit = (datos, e) =>{
+        e.preventDefault();
+        try {
+            console.log(datos);
+            axios.post(URL, datos);
+
+            
+            e.target.reset();
+        } catch (error) {
+            console.log(error);
+        }            
+    }
+
+   /* const [nombre, setNombre] = useState('');
+    const [apellido, setApellido] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState(''); */
+
+    return(
+     <>
+     <hr/>
+     <h1 className='centrar'>Formulario de Registro</h1>
+     <hr/>
+     <Form onSubmit={handleSubmit(onSubmit)}>
+        <Form.Field>
+            <label>Nombre: </label>
+            <input placeholder='Nombre'
+                   type="text"
+                   name='nombre' 
+                   {...register('nombre',
+                   {
+                    required: true,
+                    maxLength:50
+                   })
+                  }
+            />
+        </Form.Field>
+        {errors.nombre && <p className='errores'>El nombre es obligatorio y con un maximo de 50 caracteres</p>}
+       
+        <Form.Field>
+        <label>Apellido: </label>
+            <input placeholder='Apellido'
+                   type="text"
+                   name='apellido'
+                   {...register('apellido',
+                   {
+                    required: true,
+                    maxLength:80
+                   })
+                  } 
+            />
+        </Form.Field>
+        {errors.apellido && <p className='errores'>El apellido es obligatorio y con un maximo de 80 caracteres</p>}
+
+        <Form.Field>
+        <label>Email: </label>
+            <input placeholder='Email'
+                   type="email"
+                   name='email'
+                   {...register('email',
+                   {
+                    required: true,
+                    pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                   })
+                  } 
+            />
+        </Form.Field>
+        {errors.email && <p className='errores'>El email es obligatorio</p>}
+
+        <Form.Field>
+        <label>Password: </label>
+            <input placeholder='Password de 6 a 10 caracteres'
+                   type="password"
+                   name='password' 
+                   {...register('password',
+                   {
+                    required: true,
+                    pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,10}$/
+                   })
+                  } 
+            />
+        </Form.Field>
+        {errors.password && <p className='errores'>El password debe contener minusculas, mayusculas y entre 6 y 10 caracteres</p>}
+
+        <div className='centrar'>
+        <Button.Group>
+          <Button type='button' onClick={()=>reset()} primary>Limpiar Formulario</Button>
+          <Button.Or />
+          <Button type='submit' positive>Enviar Datos</Button>
+        </Button.Group>
+
+
+                { /* <button type='submit'>Enviar Datos</button>
+                     <button type='button' onClick={()=>reset()}>Limpiar Formulario</button> */ }
+        </div> 
+     </Form>
+     </> 
+    )
+}
+
+export default Formulario;
